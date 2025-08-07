64380faedeb3bdbf171ef7ba7aba6132cdf55067
KERN_SUCCESS <jakajakamd@gmail.com>
2025-08-06 21:21:20 -0500

    ignore

--- roblox.cpp
+++ roblox.cpp
@@ -3,20 +3,20 @@
 
 class rbx_mgr {
 public:
-   static void initialize() {
-       mem_mgr::get_lib_base("libroblox.so");
-   }
-   
-   static void print(const char* message) {
-       const auto rbx_print = reinterpret_cast<void(*)(int, const char*)>(
-           mem_mgr::REBASE(Addresses::rbx_print)
-       );
-       rbx_print(1, message);
-   }
+    static void initialize() {
+        mem_mgr::get_lib_base("libroblox.so");
+    }
+    
+    static void print(const char* message) {
+        const auto rbx_print = reinterpret_cast<void(*)(int, const char*)>(
+            mem_mgr::rebase(Addresses::rbx_print)
+        );
+        rbx_print(1, message);
+    }
 };
 
 __attribute__((constructor))
 void on_load() {
-   rbx_mgr::initialize();
-   rbx_mgr::print("hi");
+    rbx_mgr::initialize();
+    rbx_mgr::print("hi");
 }
